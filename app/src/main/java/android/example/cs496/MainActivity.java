426b98a46638901f1dec2f41c2bf3993298781e2
HyeongshinYoon <35055625+HyeongshinYoon@users.noreply.github.com>
2019-06-26 01:32:01 +0900

    initial setting

---
+++ app/src/main/java/android/example/cs496/MainActivity.java
@@ -0,0 +1,65 @@
+package android.example.cs496;
+
+import android.example.cs496.ui.main.TabFragment1;
+import android.example.cs496.ui.main.TabFragment2;
+import android.example.cs496.ui.main.TabFragment3;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.example.cs496.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabs;
+    private ViewPager viewPager;
+    SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializing the TabLayout;
+        tabs = findViewById(R.id.tabs);
+
+        //Initializing ViewPager
+        viewPager = findViewById(R.id.view_pager);
+        setupViewPager(viewPager);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabs));
+        tabs.setupWithViewPager(viewPager);
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+    
+    public void setupViewPager(ViewPager mViewPager) {
+        sectionsPagerAdapter.addFragment(new TabFragment1(), "Phone");
+        sectionsPagerAdapter.addFragment(new TabFragment2(), "Photos");
+        sectionsPagerAdapter.addFragment(new TabFragment3(), "???");
+        mViewPager.setAdapter(sectionsPagerAdapter);
+    }
+}
---
+++ app/src/main/java/android/example/cs496/ui/main/TabFragment3.java
@@ -0,0 +1,24 @@
+package android.example.cs496.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.example.cs496.R;
+
+public class TabFragment3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment3,container,false);
+    }
+}
